e6db499053974eceb26995f2403a739cbaa5353e
Neelesh Shetty <neelesh@prographer.com>
2020-10-31 22:26:27 +0530

    table skeleton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Table from './components/Table';
 
 function App() {
-  // const [data, setData] = useState()
-
-  // useEffect(() => {
-  //   axios.get('http://localhost:3000/db').then(response => console.log(response.data)).catch(err => console.log(err))
-  // }, [])
-
-  // return (
-  //   <h1>Hello</h1>
-  // );
+  return (
+    <Table />
+  );
 }
 
 export default App;
---
+++ src/components/columns.js
@@ -0,0 +1,50 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Block',
+        accessor: 'block'
+    },
+    {
+        Header: 'Cluster',
+        accessor: 'cluster'
+    },
+    {
+        Header: 'School Id',
+        accessor: 'schoolid'
+    },
+    {
+        Header: 'School Name',
+        accessor: 'schoolname'
+    },
+    {
+        Header: 'Category',
+        accessor: 'category'
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender'
+    },
+    {
+        Header: 'Medium',
+        accessor: 'medium_of_inst'
+    },
+    {
+        Header: 'Address',
+        accessor: 'address'
+    },
+    {
+        Header: 'Area',
+        accessor: 'area'
+    },
+    {
+        Header: 'Pincode',
+        accessor: 'pincode'
+    },
+    {
+        Header: 'Landmark',
+        accessor: 'landmark'
+    }
+]
